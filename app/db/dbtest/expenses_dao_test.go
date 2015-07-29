e63b16566542484fc407ab9abdf4b309dd45416d
Jaap Groeneveld <jaap.groeneveld@dynport.de>
2015-07-29 08:55:17 +0200

    reorder code

--- app/db/dbtest/expenses_dao_test.go
+++ app/db/dbtest/expenses_dao_test.go
@@ -12,19 +12,18 @@ func TestInsertExpense(t *testing.T) {
 	tx := BeginTx()
 	defer tx.Rollback()
 
-	var amount int
-	var description string
-
 	expense := model.NewExpense()
 	expense.Description = "some description"
 	expense.Amount = 23
 	err := db.InsertExpense(tx, expense)
 	assert.MustBeNil(t, err)
 
+	var amount int
+	var description string
 	row := tx.QueryRow("SELECT description, amount FROM expenses")
 	err = row.Scan(&description, &amount)
+
 	assert.MustBeNil(t, err)
 	assert.MustBeEqual(t, "some description", description)
 	assert.MustBeEqual(t, 23, amount)
-
 }
